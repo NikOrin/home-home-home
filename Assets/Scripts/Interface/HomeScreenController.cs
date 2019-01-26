9b4975164210af01819d95b1cac666256490c09e
Nicholas Lee <niklee@outlook.com>
2019-01-25 22:33:01 -0800

    added some test code
    none of this is final. just testing some button stuff as well as home screen stuff

---
+++ Assets/Scripts/Interface/HomeScreenController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeScreenController : MonoBehaviour
+{
+    public List<Button> Apps;
+
+    public GameObject GenericAppIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var buttonObject = Instantiate(GenericAppIcon);
+        var button = buttonObject.GetComponent<Button>();
+        button.onClick.AddListener(Test);
+        buttonObject.transform.SetParent(transform);
+        var btnRect = buttonObject.GetComponent<RectTransform>();
+        Debug.Log($"position: {btnRect.position}");
+        Debug.Log($"local position: {btnRect.localPosition}");
+        btnRect.localPosition = Vector3.zero;
+        Apps.Add(button);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Test()
+    {
+        Debug.Log("Test button was clicked");
+    }
+}
