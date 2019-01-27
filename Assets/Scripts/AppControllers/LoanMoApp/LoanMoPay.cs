b1021208aed7bfc5200d6ddba1c213876dca8586
Christopher Paz <cpaz1@ucsc.edu>
2019-01-27 11:18:08 -0800

    LoanMoApp is reasonably done.

---
+++ Assets/Scripts/AppControllers/LoanMoApp/LoanMoPay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoanMoPay : MonoBehaviour
+{
+	public GameObject button;
+	public Text txt;
+
+	public void PayOnClick()
+	{
+		txt = button.GetComponentInChildren<Text>();
+	
+		if(button.tag == "Right")
+		{
+			txt.text = "PAYED $300";
+		}
+			
+	}
+}
