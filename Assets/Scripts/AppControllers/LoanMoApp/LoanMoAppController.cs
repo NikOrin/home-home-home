ba4146030a87437ccfdea1c4b1f620074403c3d4
Christopher Paz <cpaz1@ucsc.edu>
2019-01-26 21:33:18 -0800

    LoanMoApp Part 1

---
+++ Assets/Scripts/AppControllers/LoanMoApp/LoanMoAppController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoanMoAppController : BaseAppController
+{
+	public List<GameObject> LoanMoPayPrefab;
+	public int Margin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
---
+++ Assets/Scripts/AppControllers/LoanMoApp/LoanMoPay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoanMoPay : MonoBehaviour
+{
+	public Dictionary<string, int> Friends;
+
+	void Start()
+	{
+		Friends = new Dictionary<string, int>();
+
+		Friends.Add("John", 50);
+		Friends.Add("Brent", 300);
+		Friends.Add("Chungus", 5000);
+		Friends.Add("Karl", 2);
+
+		Debug.Log(Friends);
+	}
+
+	public void PayOnClick()
+	{
+		Debug.Log("Pay!!");
+	}
+}
