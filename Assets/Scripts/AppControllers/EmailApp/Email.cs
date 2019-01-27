19040c7339c49fced0052315b6d96d08bcba41c6
nefremova <nefremova0@gmail.com>
2019-01-26 20:28:00 -0800

    added scrollview to message app

---
+++ Assets/Scripts/AppControllers/EmailApp/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Application.Email
+{
+    public class Email
+    {
+        public string Sender;
+        public string MessageText;
+        public string Subject;
+        public DateTime SentOn;
+        public Email()
+        {
+        }
+    }
+}
