d4ece3d7d444fecd9622fa6fb6fe64e02ce0cf41
Christopher Paz <cpaz1@ucsc.edu>
2019-01-27 14:38:17 -0800

    Static PhotogApp and finished a basic FlyApp that leads to Ch3ENdingd

--- Assets/Scripts/AppControllers/FlyApp/FlyAppPay.cs
+++ Assets/Scripts/AppControllers/FlyApp/FlyAppPay.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FlyAppPay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject button;
+	public Text txt;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void PayOnClick()
+	{
+		txt = button.GetComponentInChildren<Text>();
+		if (button.tag == "Right")
+		{
+			txt.text = "PAID FLIGHT";
+			SceneManager.LoadScene("Chapter3");
+		}
+	}
 }
